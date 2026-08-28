a8e0d9c7043e617de16f47bc15658dc49e44cd69
Jannis Alsbach <43481112+TheHummel@users.noreply.github.com>
2026-08-29 00:13:25 +0200

    Add: dynamicArray - submission-3

---
+++ Data Structures & Algorithms/dynamicArray/submission-3.cpp
@@ -0,0 +1,56 @@
+#include <cstring>
+class DynamicArray {
+public:
+    int* arr;
+    int cap;
+    int size;
+
+    DynamicArray(int capacity) {
+        cap = capacity;
+        arr = new int[cap];
+        size = 0;
+    }
+
+    int get(int i) {
+        return arr[i];
+    }
+
+    void set(int i, int n) {
+        arr[i] = n;
+    }
+
+    void pushback(int n) {
+        if (size==cap) {
+            resize();
+        }
+        arr[size] = n;
+        ++size;
+    }
+
+    int popback() {
+        --size;
+        return arr[size];
+    }
+
+    void resize() {
+        int newCap = 2 * cap;
+        int* newArr = new int[newCap];
+
+        /*for (int i=0; i<size; ++i) {
+            newArr[i] = arr[i];
+        }*/
+        memcpy(newArr, arr, size*sizeof(int));
+
+        delete[] arr;
+        arr = newArr;
+        cap = newCap;
+    }
+
+    int getSize() {
+        return size;
+    }
+
+    int getCapacity() {
+        return cap;
+    }
+};
